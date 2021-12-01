a017fddc4f13c238f3fee125e5bd55d4032a4a90
Fabio Alvarenga Silva <fabioasilva94@hotmail.com>
2021-12-01 14:33:15 -0300

    Iniciando exercicio3

---
+++ exercicio3.php
@@ -0,0 +1,117 @@
+<?php
+
+class Correntista
+{
+    private string $cpfCliente;
+    private float $saldo;
+
+    public function __construct(string $cpfCliente, float $saldo)
+    {
+        $this->cpfCliente = $cpfCliente;
+        $this->saldo = $saldo;
+    }
+
+    public function getCPFCliente(): string
+    {
+        return $this->cpfCliente;
+    }
+
+    public function getSaldo(): float
+    {
+        return $this->saldo;
+    }
+
+    public function setSaldo(float $saldo): void
+    {
+        $this->saldo = $saldo;
+    }
+}
+
+interface MovimentoConta
+{
+    public function getCPFCorrentista(): string;
+    public function getValorMovimento(): float;
+}
+
+interface OperacoesBanco
+{
+    public function encontraCorrentista(array $todosCorrentistas, string $cpfProcurado): ?Correntista;
+}
+
+class Transferencia implements MovimentoConta
+{
+    private $cpf;
+    private $valor;
+
+    public function __construct(string $cpf, float $valor)
+    {
+        $this->cpf = $cpf;
+        $this->valor = $valor;
+    }
+
+    public function getCPFCorrentista(): string
+    {
+        return $this->cpf;
+    }
+    public function getValorMovimento(): float
+    {
+        return $this->valor;
+    }
+}
+
+class Gerente implements OperacoesBanco
+{
+    public function encontraCorrentista(array $todosCorrentistas, string $cpfProcurado): ?Correntista
+    {
+        $correntista = NULL;
+
+        foreach ($todosCorrentistas as $correntistaProcurado) {
+            if ($correntistaProcurado->getCPFCliente() === $cpfProcurado) {
+                $correntista = $correntistaProcurado;
+                break;
+            }
+        }
+
+        return $correntista;
+    }
+}
+
+$contaFabio = new Correntista('00000000000', 10000);
+$contaCarlos = new Correntista('11111111111', 15000);
+$contaGuilherme = new Correntista('22222222222', 20000);
+
+
+$movimentoFabio = new Transferencia('00000000000', 1000);
+$movimentoCarlos = new Transferencia('11111111111', 2000);
+$movimentoGuilherme = new Transferencia('22222222222', 3000);
+
+$c = [
+    $contaFabio,
+    $contaCarlos,
+    $contaGuilherme
+];
+
+$m = [
+    $movimentoFabio,
+    $movimentoCarlos,
+    $movimentoGuilherme
+];
+
+
+function atualizarSaldo(array $todosCorrentistas, array $transferencias): array
+{
+    foreach ($transferencias as $transferencia) {
+        foreach ($todosCorrentistas as $index => $correntista) {
+            if ($transferencia->getCPFCorrentista() == $correntista->getCPFCliente()) {
+                $saldoAnterior = $correntista->getSaldo();
+                $saldoMovimento = $transferencia->getValorMovimento();
+                $saldoResultado = $saldoAnterior - $saldoMovimento;
+                $todosCorrentistas[$index]->setSaldo($saldoResultado);
+            }
+        }
+    }
+
+    return $todosCorrentistas;
+}
+
+$c = atualizarSaldo($c, $m);
