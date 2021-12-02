1df33c7522cba1aae03731ca821ee65d92e17277
Pedro <pivow@hotmail.com>
2021-12-02 15:01:30 -0300

    Melhorias ex1.3

--- src/exercicio1/MyExpenses.php
+++ src/exercicio1/MyExpenses.php
@@ -15,22 +15,34 @@ class MyExpenses {
         $cpf = $this->cpf ;
         return $cpf;
     }
-    public function totalizaMes($mes){
-        $despesas = "Despesas do mes".$mes;
-        return $despesas;
+    public function totalizaMes(int $mes){
+
+        $totalDespesasNoMes = 0;
+
+        foreach($this->despesas as $despesa){
+            if($despesa->getMes() == $mes){
+                $totalDespesasNoMes += $despesa->getValor();
+            }
+        }
+        return new DespesaMes($mes, $totalDespesasNoMes);
+        
     }
 
-    public function GravaInfos(DespesaMes $despesaMes){
-         $objSerializado = serialize($despesaMes);
+
+
+
+    public function gravaInfos(DespesaMes $despesaMes){
+
+        $objSerializado = serialize($despesaMes);
         $filename = $this->nomeDoIndividuo . ".txt";
         $file = fopen($filename, "a+");
         fwrite($file, $objSerializado);
         fclose($file);
         echo "arquivo gravado com sucesso";
-
-
     }
-
+    
 }
+
+?>
 
 
