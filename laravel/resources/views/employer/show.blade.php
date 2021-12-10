4678b815face1ffcda54ccce15ef89d879280255
Pedro <pivow@hotmail.com>
2021-12-10 14:24:59 -0300

    Correções front-end

--- laravel/resources/views/employer/show.blade.php
+++ laravel/resources/views/employer/show.blade.php
@@ -1,15 +1,15 @@
 @include('includes.header')
     <div class="container">
         <h1 class="text-primary text-center">
-                Item
+                Funcionarios
         </h1>
 
         <ul class="list-group list-group-flush text-center">
             <h3><li class="list-group-item">ID: {{$employer->id}}</li></h3>
             <h3><li class="list-group-item">Funcionaro: {{$employer->name}}</li></h3>
             <h3><li class="list-group-item">Função: {{$employer->jobRole}}</li></h3>
-            <h3><li class="list-group-item">Salário: {{$employer->jobRole}}</li></h3>
-            <h3><li class="list-group-item">Tempo de Contrato: {{$employer->serviceTime}}</li></h3>
+            <h3><li class="list-group-item">Salário:R$ {{$employer->wage}},00</li></h3>
+            <h3><li class="list-group-item">Tempo de Contrato: {{$employer->serviceTime}} anos</li></h3>
         </ul>
 
         <div class="row d-flex justify-content-center mt-2">
