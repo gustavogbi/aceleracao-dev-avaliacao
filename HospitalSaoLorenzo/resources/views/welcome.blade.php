2feee4514a6df03a08c1b07c9409ce2efc43534b
Pedro <pivow@hotmail.com>
2021-12-14 11:43:21 -0300

    Projeto Hospital Sãao Lorenzo

---
+++ HospitalSaoLorenzo/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('single')
+@section('content')
+
+Bumbum do Fabio e do Lucas é melhor do que o do Gustavo. 
+
+Bumbum do Fabio e do Lucas é melhor do que o do Gustavo. 
+
+Bumbum do Fabio e do Lucas é melhor do que o do Gustavo. 
+
+Bumbum do Fabio e do Lucas é melhor do que o do Gustavo. 
+
+Bumbum do Fabio e do Lucas é melhor do que o do Gustavo. 
+
+@endsection
