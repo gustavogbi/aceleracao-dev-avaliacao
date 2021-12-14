eb63fbc06f734940a4a1a688a3e34a825c6c78f2
Pedro <pivow@hotmail.com>
2021-12-14 20:38:09 -0300

    Projeto Hospital São Lorenzo - validações| Melhorias front

--- HospitalSaoLorenzo/resources/views/Banco/Partes/formatabancodesangue.blade.php
+++ HospitalSaoLorenzo/resources/views/Banco/Partes/formatabancodesangue.blade.php
@@ -1,17 +1,42 @@
+@if ($errors->any())
+    @foreach ($errors->all() as $error)
+    <div class="alert alert-danger">
+  <strong>Atenção!</strong> {{$error}}
+</div>
+    @endforeach
+@endif
 
 @csrf
                         <div class="form-group row">
-                            <div class="col-sm-6 mb-3 mb-sm-0">
+                            <div class="col-md-2">
                                 Tipo:
-                                <input type="text" class="form-control form-control-user" name="tipo" value="{{ $bancodesangue->tipo ?? old('tipo')}}" 
-                                    placeholder="Tipo">
+                                <select name="tipo"  class="form-control form-control-user" >
+                                    <option name="tipo" value="A+"  @if(!empty($bancodesangue))  {{ $bancodesangue->tipo == 'A+' ? 'selected': ''}} @endif  > A+ </option>
+                                     <option name="tipo" value="B+" @if(!empty($bancodesangue)) {{ $bancodesangue->tipo == 'B+' ? 'selected': ''}} @endif  > B+ </option>
+                                     <option name="tipo" value="AB+" @if(!empty($bancodesangue)) {{ $bancodesangue->tipo == 'AB+' ? 'selected': '' }} @endif  > AB+ </option>
+                                     <option name="tipo" value="O+"  @if(!empty($bancodesangue)) {{ $bancodesangue->tipo == 'O+' ? 'selected': '' }} @endif  > O+ </option>
+                                     <option name="tipo" value="A-"  @if(!empty($bancodesangue)) {{ $bancodesangue->tipo == 'A-' ? 'selected': '' }} @endif> A- </option>
+                                     <option name="tipo" value="B-"  @if(!empty($bancodesangue)) {{ $bancodesangue->tipo == 'B-' ? 'selected': '' }} @endif> B- </option>
+                                     <option name="tipo" value="AB-"  @if(!empty($bancodesangue)) {{ $bancodesangue->tipo == 'AB-' ? 'selected': '' }} @endif> AB- </option>
+                                     <option name="tipo" value="O-"  @if(!empty($bancodesangue)) {{ $bancodesangue->tipo == 'O+' ? 'selected': '' }} @endif>O-  </option>
+                                </select>
                             </div>
-                            <div class="col-sm-6">
+                            <div class="col-md-2">
                                 Contabilidade:
                                 <input type="text" class="form-control form-control-user"name="contabilidade" value="{{ $bancodesangue->contabilidade ?? old('contabilidade')}}"
-                                    placeholder="Contabilidade">
+                                    placeholder="Digite a quantidade de sangue em litros" >
+                                    
                             </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary btn-user btn-block">
+
+                            
+                            <div class="col-md-2">
+                                <br>
+                                <button type="submit" class="btn btn-primary btn-user btn-block">
                            Confirmar
                         </button>
+                                    
+                            </div>
+
+
+                            
+                        </div>
