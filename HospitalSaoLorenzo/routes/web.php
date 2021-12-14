6b03ca25b118e00b1f17163df611597e2d195f3f
Pedro <pivow@hotmail.com>
2021-12-14 20:41:36 -0300

    Projeto Hospital São Lorenzo - validações| Melhorias front

--- HospitalSaoLorenzo/routes/web.php
+++ HospitalSaoLorenzo/routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\BancoSangueController;
-use App\Http\Controllers\AcessoController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AcessoController;
+use App\Http\Controllers\DoadorController;
+use App\Http\Controllers\MedicoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +28,22 @@ Route::delete('/bancodesangue/remover/{id}', [BancoSangueController::class, 'del
 Route::get('/bancodesangue/editar/{id}', [BancoSangueController::class, 'edit'])->name('bancodesangue.editar');
 Route::put('/bancodesangue/{id}', [BancoSangueController::class, 'update'])->name('bancodesangue.update');
 Route::get('/bancodesangue/{id}', [BancoSangueController::class, 'show'])->name('bancodesangue.exibe');
+
+
+/* Doador */
+Route::get('doador', [DoadorController::class, 'index'])->name('doador.index');
+Route::get('doador/create', [DoadorController::class, 'create'])->name('doador.create');
+Route::post('doador', [DoadorController::class, 'store'])->name('doador.store');
+Route::get('doador/show/{id}', [DoadorController::class, 'show'])->name('doador.show');
+Route::get('doador/edit/{id}', [DoadorController::class, 'edit'])->name('doador.edit');
+Route::delete('doador/delete/{id}', [DoadorController::class, 'destroy'])->name('doador.destroy');
+Route::put('doador/{id}', [DoadorController::class, 'update'])->name('doador.update');
+
+/* Medico */
+Route::get('/medico', [MedicoController::class, 'index'])->name('medico.index');
+Route::get('medico/create', [MedicoController::class, 'create'])->name('medico.create');
+Route::post('/medico', [MedicoController::class, 'store'])->name('medico.store');
+Route::get('medico/show/{id}', [MedicoController::class, 'show'])->name('medico.show');
+Route::get('medico/edit/{id}', [MedicoController::class, 'edit'])->name('medico.edit');
+Route::delete('medico/delete/{id}', [MedicoController::class, 'destroy'])->name('medico.destroy');
+Route::put('medico/{id}', [MedicoController::class, 'update'])->name('medico.update');
