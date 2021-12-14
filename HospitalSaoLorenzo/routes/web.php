1a327b3ae83c8dbbae02c521d65feaab92c9672a
Pedro <pivow@hotmail.com>
2021-12-14 16:24:14 -0300

    Projeto Hospital Sãao Lorenzo- Rotas Doador

--- HospitalSaoLorenzo/routes/web.php
+++ HospitalSaoLorenzo/routes/web.php
@@ -21,9 +21,19 @@ Route::get('/', function () {
 
 /* Banco de sangue */
 Route::get('/bancodesangue', [BancoSangueController::class, 'index'])->name('bancodesangue.index');;
-Route::get('/bancodesangue/novo', [BancoSangueController::class, 'new'])->name('bancodesangue.novo');;
+Route::get('/bancodesangue/novo', [BancoSangueController::class, 'new'])->name('bancodesangue.git ');;
 Route::post('/bancodesangue/insere', [BancoSangueController::class, 'insert'])->name('bancodesangue.inserir');
 Route::delete('/bancodesangue/remover/{id}', [BancoSangueController::class, 'delete'])->name('bancodesangue.remover');
 Route::get('/bancodesangue/editar/{id}', [BancoSangueController::class, 'edit'])->name('bancodesangue.editar');
 Route::put('/bancodesangue/{id}', [BancoSangueController::class, 'update'])->name('bancodesangue.update');
 Route::get('/bancodesangue/{id}', [BancoSangueController::class, 'show'])->name('bancodesangue.exibe');
+
+
+/* Doador */
+Route::get('doador', [DoadorController::class, 'index'])->name('doador.index');
+Route::get('doador/create', [DoadorController::class, 'create'])->name('doador.create');
+Route::post('doador', [DoadorController::class, 'store'])->name('doador.store');
+Route::get('doador/show/{id}', [DoadorController::class, 'show'])->name('doador.show');
+Route::get('doador/edit/{id}', [DoadorController::class, 'edit'])->name('doador.edit');
+Route::delete('doador/delete/{id}', [DoadorController::class, 'destroy'])->name('doador.destroy');
+Route::put('doador/{id}', [DoadorController::class, 'update'])->name('doador.update');
