4b2695ddcd62f6d7831131a086098027439f362b
winicius-leal <winiciusleal@hotmail.com>
2021-12-27 12:11:09 -0300

    create model controller and migration professor

---
+++ FaculdadeVilaNova/app/Http/Controllers/ProfessorController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Professor;
+use Illuminate\Http\Request;
+
+class ProfessorController extends Controller
+{
+         
+    protected $view = 'professor';
+    protected $route = 'professores';
+
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Professor $professor)
+    {
+        //
+    }
+
+    public function edit(Professor $professor)
+    {
+        //
+    }
+
+    public function update(Request $request, Professor $professor)
+    {
+        //
+    }
+
+    public function destroy(Professor $professor)
+    {
+        //
+    }
+}
