b4449864d8b1757b7fcd8e967ef4e65b528322ab
Fabio Alvarenga Silva <fabioasilva94@hotmail.com>
2021-12-28 18:08:32 -0300

    Nav active

--- FaculdadeVilaNova/resources/views/layouts/nav.blade.php
+++ FaculdadeVilaNova/resources/views/layouts/nav.blade.php
@@ -12,68 +12,80 @@
                 </p>
             </a>
             <ul class="nav nav-treeview">
-              <li class="nav-item">
-              <a href="/cursos" class="nav-link">
-              <i class="nav-icon 	fas fa-laptop-code"></i>
-              <p>
-                Cursos<span class="badge badge-info right">
-                @php
-                    echo DB::table("cursos")->select("id")->count();    
-                   @endphp
-                </span>
-              </p>
-            </a>
-            <li class="nav-item">
-              <a href="/alunos" class="nav-link">
-              <i class="nav-icon fas fa-user-graduate"></i>
-              <p>
-                Alunos
-                <span class="badge badge-info right">
-                @php
-                    echo DB::table("alunos")->select("id")->count();    
-                   @endphp
-                </span>
-              </p>
-            </a>
-              </li>
-            <li class="nav-item">
-              <a href="{{ route('planos-financeiros.index') }}" class="nav-link">
-              <i class="nav-icon fas fa-money-bill-wave"></i>
-              <p>Plano Financeiro
-                 <span class="badge badge-info right">
-                    @php
-                    echo DB::table("planos_financeiros")->select("id")->count();    
-                   @endphp
-                </span>
-              </p>
-            </a>
-              </li>
-            <li class="nav-item">
-              <a href="{{route('professores.index')}}" class="nav-link">
-              <i class="nav-icon fas fa-chalkboard-teacher"></i>
-              <p>
-               Professores
-                <span class="badge badge-info right">
-                  @php
-                    echo DB::table("professores")->select("id")->count();    
-                  @endphp
-                </span>
-              </p>
-            </a>
-              </li>
-            <li class="nav-item">
-              <a href="" class="nav-link">
-              <i class="nav-icon fas fa-book-open"></i>
-              <p>
-               Aulas
-                <span class="badge badge-info right">
-                <span class="badge badge-info right">
-             
-                </span>
-                </span>
-              </p>
-            </a>
-              </li>
+                <li class="nav-item">
+                    <a href="{{ route('cursos.index') }}"
+                        class="nav-link {{ request()->is('cursos*') ? 'active' : '' }}">
+                        <i class="nav-icon 	fas fa-laptop-code"></i>
+                        <p>
+                            Cursos<span class="badge badge-info right">
+                                @php
+                                    echo DB::table('cursos')
+                                        ->select('id')
+                                        ->count();
+                                @endphp
+                            </span>
+                        </p>
+                    </a>
+                <li class="nav-item">
+                    <a href="{{ route('alunos.index') }}"
+                        class="nav-link {{ request()->is('alunos*') ? 'active' : '' }}">
+                        <i class="nav-icon fas fa-user-graduate"></i>
+                        <p>
+                            Alunos
+                            <span class="badge badge-info right">
+                                @php
+                                    echo DB::table('alunos')
+                                        ->select('id')
+                                        ->count();
+                                @endphp
+                            </span>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('planos-financeiros.index') }}"
+                        class="nav-link {{ request()->is('planos-financeiros*') ? 'active' : '' }}">
+                        <i class="nav-icon fas fa-money-bill-wave"></i>
+                        <p>Plano Financeiro
+                            <span class="badge badge-info right">
+                                @php
+                                    echo DB::table('planos_financeiros')
+                                        ->select('id')
+                                        ->count();
+                                @endphp
+                            </span>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('professores.index') }}"
+                        class="nav-link {{ request()->is('professores*') ? 'active' : '' }}">
+                        <i class="nav-icon fas fa-chalkboard-teacher"></i>
+                        <p>
+                            Professores
+                            <span class="badge badge-info right">
+                                @php
+                                    echo DB::table('professores')
+                                        ->select('id')
+                                        ->count();
+                                @endphp
+                            </span>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-book-open"></i>
+                        <p>
+                            Aulas
+                            <span class="badge badge-info right">
+                                <span class="badge badge-info right">
+
+                                </span>
+                            </span>
+                        </p>
+                    </a>
+                </li>
             </ul>
         </li>
     </ul>
