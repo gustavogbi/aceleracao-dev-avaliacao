697abb9b35df72907cdf45731f385a06efc144fa
Pedro <pivow@hotmail.com>
2021-12-28 18:57:33 -0300

    Menu de APIS

--- FaculdadeVilaNova/resources/views/layouts/nav.blade.php
+++ FaculdadeVilaNova/resources/views/layouts/nav.blade.php
@@ -88,6 +88,92 @@
                 </li>
             </ul>
         </li>
+
+        <li class="nav-item has-treeview  ">
+            <a href="#" class="nav-link bg-warning">
+                <i class="nav-icon fas fa-tachometer-alt"></i>
+                <p>
+                    API
+                    <i class="right fas fa-angle-left"></i>
+                </p>
+            </a>
+            <ul class="nav nav-treeview">
+                <li class="nav-item">
+                    <a href="/api/cursos"
+                        class="nav-link ">
+                        <i class="nav-icon 	fas fa-laptop-code"></i>
+                        <p>
+                            Cursos<span class="badge badge-info right">
+                                @php
+                                    echo DB::table('cursos')
+                                        ->select('id')
+                                        ->count();
+                                @endphp
+                            </span>
+                        </p>
+                    </a>
+                <li class="nav-item">
+                    <a href="/api/alunos"
+                        class="nav-link ">
+                        <i class="nav-icon fas fa-user-graduate"></i>
+                        <p>
+                            Alunos
+                            <span class="badge badge-info right">
+                                @php
+                                    echo DB::table('alunos')
+                                        ->select('id')
+                                        ->count();
+                                @endphp
+                            </span>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="/api/planos-financeiros"
+                        class="nav-link ">
+                        <i class="nav-icon fas fa-money-bill-wave"></i>
+                        <p>Plano Financeiro
+                            <span class="badge badge-info right">
+                                @php
+                                    echo DB::table('planos_financeiros')
+                                        ->select('id')
+                                        ->count();
+                                @endphp
+                            </span>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="/api/professores"
+                        class="nav-link">
+                        <i class="nav-icon fas fa-chalkboard-teacher"></i>
+                        <p>
+                            Professores
+                            <span class="badge badge-info right">
+                                @php
+                                    echo DB::table('professores')
+                                        ->select('id')
+                                        ->count();
+                                @endphp
+                            </span>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="nav-icon fas fa-book-open"></i>
+                        <p>
+                            Aulas
+                            <span class="badge badge-info right">
+                                <span class="badge badge-info right">
+
+                                </span>
+                            </span>
+                        </p>
+                    </a>
+                </li>
+            </ul>
+        </li>
     </ul>
 </nav>
 <!-- /.sidebar-menu -->
