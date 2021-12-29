3a17a1fa6750404c6bbefb2e4cf2dfe0cd364cbe
Lucas <lubapisilva@gmail.com>
2021-12-29 09:59:33 -0300

    add Aulaseeder no call Seeders

--- FaculdadeVilaNova/database/seeders/DatabaseSeeder.php
+++ FaculdadeVilaNova/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Aula;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,6 +21,7 @@ class DatabaseSeeder extends Seeder
             ProfessorSeeder::class,
             CursoSeeder::class,
             PlanoFinanceiroSeeder::class,
+            AulaSeeder::class
         ]);
     }
 }
