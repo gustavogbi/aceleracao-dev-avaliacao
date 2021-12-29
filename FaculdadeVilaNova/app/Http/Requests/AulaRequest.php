914a2f6bf7b9a858e060dc0da0165ef9e190abe0
Lucas <lubapisilva@gmail.com>
2021-12-29 05:51:45 -0300

    correcao no request de numero de aulas

--- FaculdadeVilaNova/app/Http/Requests/AulaRequest.php
+++ FaculdadeVilaNova/app/Http/Requests/AulaRequest.php
@@ -24,9 +24,9 @@ class AulaRequest extends FormRequest
     public function rules()
     {
         return [
-            'num_aula'  => 'required|integer|between:1,40',
+            'num_aula'  => 'required|integer',
             'tema' => 'required|min:5|max:45',
-            'duracao'   => 'required|min:5|max:45',
+            'duracao'   => 'required|min:1|max:3',
             'observacao'    => 'required|min:5|max:255',
             'idcurso' => 'required|integer|between:1,8',
             'idprofessor' => 'required|integer|between:1,10',
@@ -37,15 +37,15 @@ class AulaRequest extends FormRequest
     {
         return [
             'num_aula.required' => 'O campo Numero de Aula não pode ser vázio',
-            'num_aula.between' => 'É necessário colocar o numero de aulas',
+            'num_aula.integer' => 'É necessário colocar o numero de aulas em numeros',
 
             'tema.required' => 'O campo Tema não pode ser vázio',
             'tema.min' => 'O campo Tema não pode conter menos que 5 caracteres',
             'tema.max' => 'O campo Tema não pode conter mais que 45 caracteres',
 
             'duracao.required' => 'O campo duração não pode ser vázio',
-            'duracao.min' => 'O campo Duração não pode conter menos que 5 caracteres',
-            'duracao.max' => 'O campo Duração não pode conter mais que 45 caracteres',
+            'duracao.min' => 'O campo Duração não pode conter menos que 1 caracteres',
+            'duracao.max' => 'O campo Duração não pode conter mais que 3 caracteres',
 
             'observacao.required' => 'O campo Observação não pode ser vázio',
             'observacao.min' => 'O campo Obserbavação não pode conter menos que 5 caracteres',
