2dfd4ac5e4478103b692ffe0fb93168258153fd5
Pedro <pivow@hotmail.com>
2021-12-29 17:32:07 -0300

    Melhoria no formulario

--- FaculdadeVilaNova/resources/views/curso/form.blade.php
+++ FaculdadeVilaNova/resources/views/curso/form.blade.php
@@ -1,23 +1,38 @@
 @csrf
+<div class="row">
+
+<div class="col-md-8">
+    
 Curso:
 <input type="text" name="nomedocurso" id="nomedocurso" value="{{ $cad->nomedocurso ?? old('nomedocurso') }}"
     class="form-control">
-<br>
+</div>
+<div class="col-md-4">
+    
 Quant. Max.:
 <input type="text" name="qntmax" id="qntmax" value="{{ $cad->qntmax ?? old('qntmax') }}" class="form-control">
+</div>
+<div class="col-md-12">
 <br>
 Descrição:
 <input type="text" name="descricao" id="descricao" value="{{ $cad->descricao ?? old('descricao') }}"
     class="form-control">
 <br>
+</div>
+<div class="col-md-4">
+   
 Categoria:
 <input type="text" name="categoria" id="categoria" value="{{ $cad->categoria ?? old('categoria') }}"
-    class="form-control">
-<br>
+    class="form-control"> 
+</div>
+<div class="col-md-4">
+    
 Mensalidade:
 <input type="text" name="mensalidade" id="mensalidade" value="{{ $cad->mensalidade ?? old('mensalidade') }}"
     class="form-control">
-<br>
+</div>
+<div class="col-md-4">
+   
 Status:
 <select class="select2bs4" name="status" id="status" style="width: 100%;">
     <option {{ ($cad->status ?? old('status')) == 'Disponível' ? 'selected' : '' }} value="Disponível">
@@ -25,6 +40,7 @@ Status:
     </option>
     <option {{ ($cad->status ?? old('status')) == 'Indisponível' ? 'selected' : '' }} value="Indisponível">
         Indisponível</option>
-</select>
-<br>
+</select> 
+</div>
+</div><br>
 <button type="subbmit" class="btn btn-primary addCompany">Salvar</button>
