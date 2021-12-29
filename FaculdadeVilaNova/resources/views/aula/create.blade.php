5423fd77da9f7b9ac26ae091acd84ae7ce5b2e56
Pedro <pivow@hotmail.com>
2021-12-29 20:35:36 -0300

    Organização do formulario| Retorno do nome do professor e nome do curso na index

--- FaculdadeVilaNova/resources/views/aula/create.blade.php
+++ FaculdadeVilaNova/resources/views/aula/create.blade.php
@@ -8,25 +8,25 @@
                 <div class="card-header">Aula</div>
 
                 <div class="card-body">
-                <a href="{{route('aulas.index') }}"><button type="button" class="btn btn-success tet-white">
-                  <i class="fa fa-arrow-left"></i> VOLTAR</button></a>
+                    <a href="{{route('aulas.index') }}"><button type="button" class="btn btn-success tet-white">
+                            <i class="fa fa-arrow-left"></i> VOLTAR</button></a>
 
                 </div>
             </div>
         </div>
     </div>
-  </div>
+</div>
 <div class="container">
     <div class="row ">
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Dados</div>
-        @include('layouts.alerts')
+                @include('layouts.alerts')
                 <div class="card-body">
-                <form action="{{ route('aulas.store') }}" method="post">
-        @method('POST')
-                @include('aula.form')
-                   </form>
+                    <form action="{{ route('aulas.store') }}" method="post">
+                        @method('POST')
+                        @include('aula.form')
+                    </form>
                 </div>
             </div>
         </div>
