7b8654e94bc3acffe9b97a26579156541af3fb60
Lucas <lubapisilva@gmail.com>
2021-12-29 05:51:03 -0300

    funcao de destroy implementanda

--- FaculdadeVilaNova/app/Http/Controllers/AulaController.php
+++ FaculdadeVilaNova/app/Http/Controllers/AulaController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AulaRequest;
 use App\Models\Aula;
-use App\Http\Requests\StoreAulaRequest;
-use App\Http\Requests\UpdateAulaRequest;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Request as FacadesRequest;
 
 class AulaController extends Controller
 {
@@ -26,38 +23,21 @@ class AulaController extends Controller
         return view($this->view.'.index', compact('cads'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view($this->view.'.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreAulaRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreAulaRequest $request)
+    public function store(AulaRequest $request)
     {
         $cad = Aula::create($request->all());
 
-        return view($this->view.'.update', compact('cad'))->with('success', "Cadastrado efetivado com sucesso!");
+        return view($this->view.'.index', compact('cad'))->with('success', "Cadastrado efetivado com sucesso!");
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Aula  $aula
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        $cad = Aula::find($id);
+        $cad = Aula::findOrFail($id);
 
         if($cad)
         {
@@ -67,15 +47,9 @@ class AulaController extends Controller
         return redirect()->back();
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Aula  $aula
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        $cad = Aula::find($id);
+        $cad = Aula::findOrFail($id);
 
         if(!$cad):
             return redirect()->back();
@@ -84,33 +58,24 @@ class AulaController extends Controller
         return view($this->view.'.update', compact('cad'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateAulaRequest  $request
-     * @param  \App\Models\Aula  $aula
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateAulaRequest $request, $id)
+    public function update(AulaRequest $request, $id)
     {
-        $cad = Aula::find($id);
+        $cad = Aula::findOrFail($id);
         if(!$cad):
             return redirect()->back();
         endif;
 
         $cad->update($request->all());
-        $id= $cad->id;
-        return view($this->view.'.update', compact('cad'))->with('success', "Cadastrado efetivado com sucesso!");
+        $id = $cad->id;
+
+        return redirect()->route($this->route . '.index')->with('success', "Cadastrado atualizado com sucesso!");
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Aula  $aula
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Aula $aula)
+    public function destroy($id)
     {
-        //
+        $cad = Aula::findOrFail($id);
+        $cad->delete();
+
+        return redirect()->route($this->route . '.index')->with('danger', "Cadastro deletado com sucesso");
     }
 }
